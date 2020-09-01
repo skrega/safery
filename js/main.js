28f200e142880e318f6517404bced1e106246e7f
skrega <skorpionn440@gmail.com>
2020-09-01 17:20:21 +1000

    added first page

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    // $('select').styler();
+    // $('.stages__item').hover(
+    //     function(){ 
+    //         $(this).find('.stages__item-img').toggleClass('visible');
+    //     }
+
+    // );
+
+    $('select').selectric();
+});
