5fc0736a81cf63c654d685a55dae543f840d11a7
skrega <skorpionn440@gmail.com>
2020-09-01 19:45:40 +1000

    fixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $(function(){
-    // $('select').styler();
-    // $('.stages__item').hover(
-    //     function(){ 
-    //         $(this).find('.stages__item-img').toggleClass('visible');
-    //     }
-
-    // );
-
+    
     $('select').selectric();
+
 });
